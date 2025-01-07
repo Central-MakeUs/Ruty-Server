3b7ab29ccfe079543853f7824bb1ff19ad705393
singsangssong <plmko0914@gmail.com>
2025-01-07 17:52:58 +0900

    health check commit

---
+++ src/main/java/com/luty/lutyserver/HealthController.java
@@ -0,0 +1,15 @@
+package com.luty.lutyserver;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@NoArgsConstructor
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> checkHealth() {
+        return ResponseEntity.ok("healthy!!");
+    }
+}
