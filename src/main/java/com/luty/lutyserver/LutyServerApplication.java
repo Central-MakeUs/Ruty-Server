7f705b0f8c67c3c6f5018af02d6a6ecf9d1d6b13
singsangssong <plmko0914@gmail.com>
2025-01-07 16:43:07 +0900

    write README

---
+++ src/main/java/com/luty/lutyserver/LutyServerApplication.java
@@ -0,0 +1,13 @@
+package com.luty.lutyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LutyServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luty/lutyserver/LutyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luty.lutyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LutyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
